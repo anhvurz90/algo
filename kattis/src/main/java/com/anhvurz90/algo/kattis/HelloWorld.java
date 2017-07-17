3b2d51b3bde3e63083db158b6268ce179d04a602
anhvurz90 <anhvurz90@gmail.com>
2017-07-17 14:28:33 +0700

    Add kattis HelloWorld

---
+++ kattis/src/main/java/com/anhvurz90/algo/kattis/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.anhvurz90.algo.kattis;
+
+import java.io.FileNotFoundException;
+
+public class HelloWorld {
+    public static void main(String[] args) throws FileNotFoundException {
+        new HelloWorld().run();
+    }
+    
+    private void run() throws FileNotFoundException {
+        System.out.println("Hello World!");
+    }
+}
