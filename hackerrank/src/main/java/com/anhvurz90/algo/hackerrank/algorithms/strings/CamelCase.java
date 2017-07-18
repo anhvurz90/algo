24c26ad8839b5424d8a3f590dd13a9a8b01d9186
anhvurz90 <anhvurz90@gmail.com>
2017-07-18 17:18:44 +0700

    Add HackerRank.Algo.Strings.CamelCase

---
+++ hackerrank/src/main/java/com/anhvurz90/algo/hackerrank/algorithms/strings/CamelCase.java
@@ -0,0 +1,20 @@
+package com.anhvurz90.algo.hackerrank.algorithms.strings;
+
+import java.util.Scanner;
+
+public class CamelCase {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isUpperCase(s.charAt(i))) {
+                count ++;
+            }
+        }
+        System.out.println(count + 1);
+        in.close();
+    }
+    
+}
