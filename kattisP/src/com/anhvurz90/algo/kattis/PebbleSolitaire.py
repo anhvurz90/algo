dffdb19d3dcb9d2bfa1aaace13b14739de7e011a
anhvurz90 <anhvurz90@gmail.com>
2017-08-06 22:41:41 +0700

    Add kattisP.PebbleSolitaire

---
+++ kattisP/src/com/anhvurz90/algo/kattis/PebbleSolitaire.py
@@ -0,0 +1,58 @@
+'''
+Created on Aug 6, 2017
+
+@author: anhvu
+'''
+global f;
+global x;
+x - '-';
+global o;
+o = 'o';
+n = int(input());
+m = pow(2, 12);
+
+f = [-1 for i in range(m)];
+f[0] = 0;
+
+def toInt(game):
+    global o;
+    ret = 0;
+    for c in game:
+        ret *= 2;
+        if (c == o):
+            ret += 1;
+    return ret;
+
+def canMove(game):
+    global x;
+    global o;
+    for i in range(game) - 2:
+        if (game[i] == x) & (game[i+1] == o) & (game[i+2] == o):
+            return True;
+        if (game[i] == o) & (game[i+1] == o) & (game[i+2] == x):
+            return True;
+    return False;
+    
+def countPebble(game):
+    global o;
+    ret = 0;
+    for i in range(game):
+        if (game[i] == o):
+            ret += 1;
+    return ret;
+def findF(game):
+    num = toInt(game);
+    if f[num] == -1:
+        if not canMove(game):
+            f[num] = countPebble(game);
+        else:
+            #checkMoveLeft
+            
+            #checkMoveRight
+    return f[num];
+    
+for i in range(n):
+    game = input();
+    print(findF(game));
+
+
