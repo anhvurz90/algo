2ffc3ab6864911adce8bdfd7270625b955b147e5
anhvurz90 <anhvurz90@gmail.com>
2017-08-07 09:58:33 +0700

    Finish kattisP.PebbleSolitaire

--- kattisP/src/com/anhvurz90/algo/kattis/PebbleSolitaire.py
+++ kattisP/src/com/anhvurz90/algo/kattis/PebbleSolitaire.py
@@ -5,7 +5,7 @@ Created on Aug 6, 2017
 '''
 global f;
 global x;
-x - '-';
+x = '-';
 global o;
 o = 'o';
 n = int(input());
@@ -26,7 +26,7 @@ def toInt(game):
 def canMove(game):
     global x;
     global o;
-    for i in range(game) - 2:
+    for i in range(len(game) - 2):
         if (game[i] == x) & (game[i+1] == o) & (game[i+2] == o):
             return True;
         if (game[i] == o) & (game[i+1] == o) & (game[i+2] == x):
@@ -36,19 +36,30 @@ def canMove(game):
 def countPebble(game):
     global o;
     ret = 0;
-    for i in range(game):
+    for i in range(len(game)):
         if (game[i] == o):
             ret += 1;
     return ret;
 def findF(game):
+    global x;
+    global o;
     num = toInt(game);
     if f[num] == -1:
         if not canMove(game):
             f[num] = countPebble(game);
         else:
+            ret = len(game);
+            for i in range(len(game) - 2):
             #checkMoveLeft
-            
+                #print(game, i);
+                if (game[i] == x) & (game[i+1] == o) & (game[i+2] == o):
+                    game1 = game[:i] + o + x + x + game[i+3:];
+                    ret = min(ret, findF(game1));
             #checkMoveRight
+                if (game[i] == o) & (game[i+1] == o) & (game[i+2] == x):
+                    game2 = game[:i] + x + x + o + game[i+3:];
+                    ret = min(ret, findF(game2));
+            f[num] = ret;
     return f[num];
     
 for i in range(n):
