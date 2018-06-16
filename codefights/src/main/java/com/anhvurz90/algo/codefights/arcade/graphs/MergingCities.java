e0c96c549e9e6396204beac157163accaba53508
Nguyen Anh Vu <anhvurz90@gmail.com>
2018-06-16 15:34:15 +0700

    Add codefights.arcade.graphs.MergingCities

---
+++ codefights/src/main/java/com/anhvurz90/algo/codefights/arcade/graphs/MergingCities.java
@@ -0,0 +1,35 @@
+package com.anhvurz90.algo.codefights.arcade.graphs;
+
+public class MergingCities {
+
+	boolean[][] mergingCities(boolean[][] roadRegister) {
+		int len = roadRegister.length;
+		int mergeCount = 0;
+		int[] names = new int[len];
+
+		for (int i = 0; i < len; i++) {
+			names[i] = i;
+		}
+
+		for (int i = 0; i < len - 1; i++) {
+			if ((i % 2 == 0) && roadRegister[i][i + 1]) {
+				mergeCount++;
+				for (int b = i + 1; b < len; b++) {
+					names[b]--;
+				}
+			}
+		}
+		int newLen = len - mergeCount;
+		boolean[][] newRegister = new boolean[newLen][newLen];
+
+		for (int a = 0; a < len; a++) {
+			for (int b = 0; b < len; b++) {
+				if (roadRegister[a][b] && names[a] != names[b]) {
+					newRegister[names[a]][names[b]] = true;
+				}
+			}
+		}
+
+		return newRegister;
+	}
+}
